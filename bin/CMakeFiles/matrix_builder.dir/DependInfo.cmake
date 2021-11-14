25741f3db6edbe191c90bac7d9cbb3b314abe500
David Stanley <dmstanley@me.com>
2021-11-14 02:00:50 -0600

    Clean Branch

---
+++ bin/CMakeFiles/matrix_builder.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/davidstanley/Documents/Github/adaptive_picard_chebyshev_C/adaptive_picard_chebyshev_v4/src/c_functions.cpp" "CMakeFiles/matrix_builder.dir/c_functions.cpp.o" "gcc" "CMakeFiles/matrix_builder.dir/c_functions.cpp.o.d"
+  "/Users/davidstanley/Documents/Github/adaptive_picard_chebyshev_C/adaptive_picard_chebyshev_v4/src/chebyshev.cpp" "CMakeFiles/matrix_builder.dir/chebyshev.cpp.o" "gcc" "CMakeFiles/matrix_builder.dir/chebyshev.cpp.o.d"
+  "/Users/davidstanley/Documents/Github/adaptive_picard_chebyshev_C/adaptive_picard_chebyshev_v4/src/clenshaw_curtis_ivpII.cpp" "CMakeFiles/matrix_builder.dir/clenshaw_curtis_ivpII.cpp.o" "gcc" "CMakeFiles/matrix_builder.dir/clenshaw_curtis_ivpII.cpp.o.d"
+  "/Users/davidstanley/Documents/Github/adaptive_picard_chebyshev_C/adaptive_picard_chebyshev_v4/src/lsq_chebyshev_fit.cpp" "CMakeFiles/matrix_builder.dir/lsq_chebyshev_fit.cpp.o" "gcc" "CMakeFiles/matrix_builder.dir/lsq_chebyshev_fit.cpp.o.d"
+  "/Users/davidstanley/Documents/Github/adaptive_picard_chebyshev_C/adaptive_picard_chebyshev_v4/src/matrix_builder.cpp" "CMakeFiles/matrix_builder.dir/matrix_builder.cpp.o" "gcc" "CMakeFiles/matrix_builder.dir/matrix_builder.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
