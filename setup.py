54e5d4b6905feaaa2fd0de806f03a0b75f35f703
MGStan0 <mgmgstan@gmail.com>
2022-01-19 16:30:57 -0600

    turn on compiling for mac os and linux again

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from glob import glob
+import sys
+from pybind11 import get_cmake_dir
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension
+
 __version__ = "0.0.1"
-cpp_args = ['-std=c++11']
 ext_modules = [
     Pybind11Extension(
         "APCexample",
-        sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
-        include_dirs=["include"],
-        extra_compile_args = cpp_args,
-        cxx_std = 11
+        sorted(glob("./src/*.cpp")),  # Sort source files for reproducibility
+        include_dirs=["./include"],
+        # Example: passing in the version to the compiled code
+        #efine_macros = [('VERSION_INFO', __version__)],
     ),
 ]
 
@@ -19,7 +22,10 @@ setup(
     author="David Stanley",
     author_email = "davidms4@illinois.edu",
     description = "Test compilation",
-    ext_modules = ext_modules,
-    zip_safe = False,
-    python_requires = ">=3.6",
+    long_description="",
+    ext_modules=ext_modules,
+    # Currently, build_ext only provides an optional "highest supported C++
+    # level" feature, but in the future it may provide more features.
+    zip_safe=False,
+    python_requires=">=3.6",
 )
