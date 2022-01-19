9e943522f79b682f3e58adf1444d848e482e77a4
MGStan0 <mgmgstan@gmail.com>
2022-01-19 16:59:39 -0600

    Revert "revert include file change in APyC.cpp"
    This reverts commit 81585dd40a687159ba7ddf1341618e7626babbbe.

--- src/APyC.cpp
+++ src/APyC.cpp
@@ -14,7 +14,6 @@
 #include <time.h> 
 #include <errno.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 
 
 int APyC(std::vector<double> r, std::vector<double> v, double t0, double tf){
