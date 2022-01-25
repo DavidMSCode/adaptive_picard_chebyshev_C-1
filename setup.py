4189c634b2b19e7424d70fe53f22d2cfd277d09a
DavidMSCode <89414428+DavidMSCode@users.noreply.github.com>
2022-01-24 18:22:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
+from distutils.command.build_ext import build_ext
 from glob import glob
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
+import os
 __version__ = "0.0.1"
-cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
+extra_args = []
+if os.name != 'nt':
+    extra_args.append('-std=c++11')
+
 ext_modules = [
     Pybind11Extension(
-        "APCexample",
+        "APC",
         sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
         include_dirs=["include"],
-        extra_compile_args = cpp_args,
-        cxx_std = 11
+        extra_compile_args=extra_args,
     ),
 ]
 
 setup(
-    name = "APCexample",
+    name = "APC",
     version = __version__,
+    cmdclass={"build_ext": build_ext},
     author="David Stanley",
     author_email = "davidms4@illinois.edu",
     description = "Test compilation",
